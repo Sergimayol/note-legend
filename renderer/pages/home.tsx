91fd56344218aa57f412dd69944bb6100be9d707
unknown <sergi2808@gmail.com>
2022-10-04 17:04:21 +0200

    del some boilerplate

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 
 import type { NextPage } from "next";
 
@@ -7,19 +6,7 @@ const Home: NextPage = () => {
   return (
     <React.Fragment>
       <div className="grid grid-col-1 text-2xl w-full text-center">
-        <img className="ml-auto mr-auto" src="/images/logo.png" />
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className="mt-1 w-full flex-wrap flex justify-center">
-        <Link href="/next">
-          <a className="btn-blue">Go to next page</a>
-        </Link>
+        Home Page
       </div>
     </React.Fragment>
   );
