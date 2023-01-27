e208a016932f07e337600466b5d9e57cb7e6d18e
unknown <sergi2808@gmail.com>
2023-01-27 23:22:59 +0100

    md + katex renderer

--- renderer/components/viewnote.tsx
+++ renderer/components/viewnote.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import rehypeRaw from "rehype-raw";
-import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import ReactMarkdown from "react-markdown";
-import oneDark from "react-syntax-highlighter/dist/cjs/styles/prism/one-dark";
+import { useState } from "react";
+import rehypeRaw from "rehype-raw";
+import rehypeKatex from "rehype-katex";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 import { ViewNoteProps } from "../types/types";
+import oneDark from "react-syntax-highlighter/dist/cjs/styles/prism/one-dark";
+import "katex/dist/katex.min.css";
 
 const ViewNote: React.FC<ViewNoteProps> = ({ filename, content }) => {
   const [note, setNote] = useState<string | undefined>(content);
@@ -12,15 +15,15 @@ const ViewNote: React.FC<ViewNoteProps> = ({ filename, content }) => {
   return (
     <div>
       <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypeRaw]}
+        remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeRaw, rehypeKatex]}
         components={{
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
             return !inline && match ? (
               <SyntaxHighlighter
                 children={String(children).replace(/\n$/, "")}
-                style={oneDark}
+                style={oneDark as any}
                 language={match[1]}
                 PreTag="div"
                 {...props}
